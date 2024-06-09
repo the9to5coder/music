2892f9d2512b592d4463deadcbfa3c674967d167
Bhawan sharma <bhawansharma15@gmail.com>
2024-06-10 02:00:05 +0530

    commit

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Gabru Da Na Te",
+    artist: "the9to5coder",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Mitran Te Case",
+    artist: "the9to5coder",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Charche Ch Naam",
+    artist: "the9to5coder",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Nain Tere Chain Mere",
+    artist: "the9to5coder",
+    img: "img4",
+    audio: "music4"
+  }
+]
